af4196a3fcbee1d8871087fd3a6e63bfac08f138
Blazej Golinski <blazej.golinski@virtualclinic.direct>
2019-11-24 17:46:17 +0000

    initial commit

---
+++ library-feedback-service/src/main/java/com/arkinem/libraryfeedbackservice/LibraryFeedbackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arkinem.libraryfeedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryFeedbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryFeedbackServiceApplication.class, args);
+	}
+
+}
---
+++ library-feedback-service/src/test/java/com/arkinem/libraryfeedbackservice/LibraryFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkinem.libraryfeedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryFeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
